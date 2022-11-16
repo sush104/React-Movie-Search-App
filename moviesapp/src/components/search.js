dda1a94ff0e1e7c62737fa3a7c3f85a91f80c34d
sush104 <sushant.shelke104@gmail.com>
2022-11-16 16:05:10 +0000

    Changes in style

--- moviesapp/src/components/search.js
+++ moviesapp/src/components/search.js
@@ -4,7 +4,6 @@ const Search = (props) => {
 	return (
 		<div className='grid-search'>
 			<input
-				className='form-control'
 				value={props.value}
 				onChange={(event) => props.setSearchValue(event.target.value)}
 				placeholder='Type to search...'
